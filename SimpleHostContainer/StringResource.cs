efde4f16fa573039bbd91f3aa06810b418479330
许阳 <mkswrod@126.com>
2023-03-29 19:41:32 +0800

    StringResource与转化器

---
+++ SimpleHostContainer/StringResource.cs
@@ -0,0 +1,153 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHostContainer
+{
+    /// <summary>
+    /// 字符串资源类
+    /// </summary>
+    [JsonConverter(typeof(StringResourceConverter))]
+    public class StringResource
+    {
+        private readonly Dictionary<string, string> _textMap = new Dictionary<string, string>();
+        private CultureInfo _culture = new CultureInfo("zh-CN");
+        private bool _isDefault = false;
+
+        /// <summary>
+        /// 字符串索引器
+        /// </summary>
+        /// <param name="key">索引</param>
+        /// <returns>字符串</returns>
+        public string this[string key]
+        {
+            get
+            {
+                if (_textMap.ContainsKey(key))
+                {
+                    return _textMap[key];
+                }
+                else
+                {
+                    return key;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 格式化字符串索引器
+        /// </summary>
+        /// <param name="key">索引</param>
+        /// <param name="args">参数列表</param>
+        /// <returns>格式化后的字符串</returns>
+        public string this[string key, params object[] args]
+        {
+            get
+            {
+                if (_textMap.ContainsKey(key))
+                {
+                    string format = _textMap[key];
+
+                    return string.Format(format, args);
+                }
+                else
+                {
+                    return key;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 获取语言信息
+        /// </summary>
+        public CultureInfo Culture => _culture;
+
+        /// <summary>
+        /// 获取字符串资源
+        /// </summary>
+        public IReadOnlyDictionary<string, string> Texts => _textMap;
+
+        /// <summary>
+        /// 获取一个值来标记是否是默认字符串资源
+        /// </summary>
+        public bool IsDefault => _isDefault;
+
+        /// <summary>
+        /// 字符串资源合并
+        /// </summary>
+        /// <param name="resource">另一个实例</param>
+        public void Merge(StringResource resource)
+        {
+            List<string> keys = new List<string>(_textMap.Keys);
+
+            foreach (var key in resource._textMap.Keys)
+            {
+                if (!keys.Contains(key))
+                {
+                    keys.Add(key);
+                }
+            }
+
+            foreach (var key in keys)
+            {
+                if (!_textMap.ContainsKey(key))
+                {
+                    _textMap.Add(key, resource._textMap[key]);
+                }
+                else if (!resource._textMap.ContainsKey(key))
+                {
+                    resource._textMap.Add(key, _textMap[key]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 字符串资源帮助类
+        /// </summary>
+        internal static class Helper
+        {
+            /// <summary>
+            /// 设置语言
+            /// </summary>
+            /// <param name="resource"><see cref="StringResource"/>实例</param>
+            /// <param name="culture">语言信息实例</param>
+            public static void SetCulture(StringResource resource, CultureInfo culture)
+            {
+                resource._culture = culture;
+            }
+
+            /// <summary>
+            /// 添加字符串资源
+            /// </summary>
+            /// <param name="resource"><see cref="StringResource"/>实例</param>
+            /// <param name="key">字符串键值</param>
+            /// <param name="value">字符串</param>
+            /// <exception cref="Exception"/>
+            public static void AddText(StringResource resource, string key, string value)
+            {
+                if (!resource._textMap.ContainsKey(key))
+                {
+                    resource._textMap.Add(key, value);
+                }
+                else
+                {
+                    throw new Exception($"键值【{key}】已经存在！");
+                }
+            }
+
+            /// <summary>
+            /// 设置默认标记
+            /// </summary>
+            /// <param name="resouces"><see cref="StringResource"/>实例</param>
+            /// <param name="isDefault">默认标记值</param>
+            public static void SetIsDefault(StringResource resouces, bool isDefault)
+            {
+                resouces._isDefault = isDefault;
+            }
+        }
+    }
+}
---
+++ SimpleHostContainer/StringResourceConverter.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Globalization;
+
+namespace SimpleHostContainer
+{
+    /// <summary>
+    /// 字符串资源转换类
+    /// </summary>
+    internal class StringResourceConverter : JsonConverter
+    {
+        /// <inheritdoc/>
+        public override bool CanRead => true;
+
+        /// <inheritdoc/>
+        public override bool CanWrite => true;
+
+        /// <inheritdoc/>
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(StringResource).IsAssignableFrom(objectType);
+        }
+
+        /// <inheritdoc/>
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (CanConvert(objectType))
+            {
+                var jsonObj = JObject.Load(reader);
+                StringResource value = new StringResource();
+                JToken token = null;
+
+                if (jsonObj.TryGetValue(nameof(value.Culture), out token))
+                {
+                    if (token != null)
+                    {
+                        string culture_name = token.Value<string>();
+                        CultureInfo culture = new CultureInfo(culture_name);
+                        StringResource.Helper.SetCulture(value, culture);
+                        token = null;
+                    }
+                }
+
+                if (jsonObj.TryGetValue(nameof(value.Texts), out token))
+                {
+                    if (token != null)
+                    {
+                        foreach (var child in token.Children())
+                        {
+                            JProperty property = child as JProperty;
+                            StringResource.Helper.AddText(value, property.Name, property.Value.Value<string>());
+                        }
+                    }
+                }
+
+                return value;
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        /// <inheritdoc/>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            StringResource resource = value as StringResource;
+
+            if (resource != null)
+            {
+                writer.WriteStartObject();
+                writer.WritePropertyName(nameof(resource.Culture));
+                writer.WriteValue(resource.Culture.Name);
+                writer.WritePropertyName(nameof(resource.Texts));
+                writer.WriteStartObject();
+
+                foreach (string key in resource.Texts.Keys)
+                {
+                    writer.WritePropertyName(key);
+                    writer.WriteValue(resource.Texts[key]);
+                }
+
+                writer.WriteEndObject();
+                writer.WriteEndObject();
+            }
+        }
+    }
+}
