4f7402a9e8f6428e4e148bdf3c82a24b12398441
许阳 <mkswrod@126.com>
2023-03-29 20:04:53 +0800

    安装信息类

---
+++ SimpleHostContainer/SetupInfo.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SimpleHostContainer
+{
+    /// <summary>
+    /// 安装信息抽像类
+    /// </summary>
+    public abstract class SetupInfo : ISetupInfo
+    {
+        /// <inheritdoc/>
+        public virtual void ConfigureServices(IServiceCollection services)
+        {
+        }
+    }
+}
