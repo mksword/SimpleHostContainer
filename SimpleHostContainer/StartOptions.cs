0dd5dbb3c24ec239d06812fb1f8d0892a64f2e46
许阳 <mkswrod@126.com>
2023-03-29 20:07:43 +0800

    StartOptions

---
+++ SimpleHostContainer/StartOptions.cs
@@ -0,0 +1,17 @@
+﻿namespace SimpleHostContainer
+{
+    /// <summary>
+    /// 启动选项
+    /// </summary>
+    public class StartOptions
+    {
+        /// <summary>
+        /// 获取或设置一个布尔值来表示是否激活插件功能
+        /// </summary>
+        public bool IsEnablePlugins { get; set; }
+        /// <summary>
+        /// 获取或设置插件根目录
+        /// </summary>
+        public string PluginsRootPath { get; set; } = "Plugins";
+    }
+}
